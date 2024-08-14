619f6e483b4c11dfa7dc436f403a288376d82523
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2024-08-14 09:16:53 +0300

    recent chs

--- constants/baseUrl.ts
+++ constants/baseUrl.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = " http://192.168.37.123:8000/";
-export const MEDIA_URL = " http://192.168.37.123:8000";
+export const BASE_URL = "http://192.168.10.123:8000/";
+export const MEDIA_URL = "http://192.168.10.123:8000";
